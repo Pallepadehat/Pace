67adf7c2082064b7a04e605b15dabd746c869860
Pallepadehat <patrick.jakobsen@icloud.com>
2025-08-08 18:12:37 +0200

    Initial Commit

---
+++ Pace/PaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceApp.swift
+//  Pace
+//
+//  Created by Patrick Jakobsen on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
